05df173ae3c7308a8ae714fa1a02ce1cf3e898a9
CarlaOnate <carlaonategardella@gmail.com>
2020-01-18 21:12:39 -0600

    Base panning para separar audio, usar esto si deja de servir el audio, si marca error CORPS abrir google chrome desde termianl con open /Applications/Google\ Chrome.app --args --allow-file-access-from-file

---
+++ script.js
@@ -0,0 +1,18 @@
+
+window.onload = function() {
+    document.getElementById("start-button").onclick = function() {
+        let audioCtx = new AudioContext()
+        let htmlAudio = document.createElement('audio')
+        htmlAudio.src = 'AudioSources/poker-chips-daniel_simon.mp3'
+        htmlAudio.crossOrigin = 'anonymous'
+        let source = audioCtx.createMediaElementSource(htmlAudio)
+        let panNode = audioCtx.createStereoPanner()
+        audioCtx.destination
+        source.connect(panNode)
+        panNode.connect(audioCtx.destination)
+        panNode.pan.value = -1
+        htmlAudio.play()
+        console.log(panNode.pan)
+
+    }
+}
